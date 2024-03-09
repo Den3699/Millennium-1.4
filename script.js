64e92bbd1d8e32ceb4271eba3175581b232260d0
Budkov Denis <Chukcha2017@bk.ru>
2024-03-09 19:15:51 +0400

    Планшет финал

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const slaider = document.querySelectorAll(".card__slaider");
-const slaiderButton = document.querySelector(".inform__button-arrow");
+// const slaider = document.querySelectorAll(".card__slaider");
+// const slaiderButton = document.querySelector(".inform__button-arrow");
 
-console.log(slaider);
+// console.log(slaider);
 
-slaiderButton.addEventListener("click", function () {
-  slaider.forEach((el) => {
-    el.classList.toggle("card__slider_test");
-  });
-  slaiderButton.classList.toggle("inform__button-arrow_close");
-});
+// slaiderButton.addEventListener("click", function () {
+//   slaider.forEach((el) => {
+//     el.classList.toggle("card__slider_test");
+//   });
+//   slaiderButton.classList.toggle("inform__button-arrow_close");
+// });
+
+let card = document.querySelectorAll('.card');
+let slaiderButton = document.querySelector('.inform__button-arrow');
+
+slaiderButton.addEventListener('click', function () {
+  card.forEach(card =>
+    card.classList.toggle('card__slaider'));
+
+    slaiderButton.classList.toggle('inform__button-arrow_close');
+
+}); 
